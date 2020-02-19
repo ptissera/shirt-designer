a41dd6328ee0090834f4102027b736e432ecfbf4
Pablo Tissera <tissera.pablo@gmail.com>
2020-02-18 22:57:35 -0300

    POC completed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shirt-designer'`, () => {
+  it(`should have as title 't-shirt-designer'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('shirt-designer');
+    expect(app.title).toEqual('t-shirt-designer');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('shirt-designer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('t-shirt-designer app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shirt-designer';
+  title = 't-shirt-designer';
+   colorList = [
+    {name: 'color 1', color: '#dacd4a'},
+    {name: 'color 2', color: '#293e7a'},
+    {name: 'color 3', color: '#1e263f'},
+    {name: 'color 4', color: '#ffffff'},
+    {name: 'color 5', color: '#6d0b1c'},
+    {name: 'color 6', color: '#6b96a6'},
+    {name: 'color 7', color: '#4d8fcc'},
+    {name: 'color 8', color: '#b32753'},
+    {name: 'color 9', color: '#909195'},
+    {name: 'color1 0', color: '#58585a'},
+    {name: 'color 11', color: '#786092'},
+    {name: 'color 12', color: '#62472d'},
+    {name: 'color 13', color: '#d56326'},
+    {name: 'color 14', color: '#000000'},
+    {name: 'color 15', color: '#c72424'},
+    {name: 'color 16', color: '#b02575'},
+    {name: 'color 17', color: '#54988c'},
+    {name: 'color 18', color: '#468146'},
+    {name: 'color 19', color: '#263e32'},
+    {name: 'color 20', color: '#874997'}
+ ];
+
+  color = ['#000', '#000', '#000', '#000', '#fff'];
+
+    constructor() {
+    this.color[0]=this.colorList[1].color;
+    this.color[1]=this.colorList[4].color;
+    this.color[2]=this.colorList[8].color;
+    this.color[3]=this.colorList[12].color;
+    this.color[4]=this.colorList[13].color;
+    }
+
+    selectColor(index, color) {
+      this.color[index] = color;
+    }
 }
